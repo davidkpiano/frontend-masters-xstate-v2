2b1b58db25cd62ef42f2fc866d54daa4979051e8
Paul Coroneos <Paul.Coroneos.Contractor@hilton.com>
2021-10-21 11:42:45 -0500

    clear out example 1 createMachine() implementation

--- 01-states-transitions/main.js
+++ 01-states-transitions/main.js
@@ -10,26 +10,7 @@ import { inspect } from '@xstate/inspect';
 //   url: 'https://stately.ai/viz?inspect',
 // });
 
-const playerMachine = createMachine({
-  initial: 'loading',
-  states: {
-    loading: {
-      on: {
-        LOADED: { target: 'playing' },
-      },
-    },
-    paused: {
-      on: {
-        PLAY: { target: 'playing' },
-      },
-    },
-    playing: {
-      on: {
-        PAUSE: { target: 'paused' },
-      },
-    },
-  },
-});
+const playerMachine = createMachine({});
 
 const service = interpret(playerMachine, { devTools: true }).start();
 

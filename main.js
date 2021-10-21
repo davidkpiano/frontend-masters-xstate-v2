9b28d29500fc07cd3099586d35a2251d2f7a6f13
David Khourshid <davidkpiano@gmail.com>
2021-10-21 06:53:09 -0500

    Scratch pad

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
 import './style.css';
-
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vit!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+import { createMachine } from 'xstate';

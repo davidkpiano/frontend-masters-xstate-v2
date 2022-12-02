60efc29020d5db38671187df06c04f478db9b476
jian108 <eric.jiang@pnnl.gov>
2022-12-02 10:33:26 -0800

    00 and 01

--- 00-modeling/main.js
+++ 00-modeling/main.js
@@ -6,3 +6,41 @@ import '../style.css';
 
 // Also, come up with a simple way to "interpret" it, and
 // make it an object that you can `.send(...)` events to.
+
+const initialState = {
+    value: 'loading', // or 'playing' or 'paused'
+  };
+
+const playerMachineObject = {
+    initial: 'loading',
+    states: {
+        loading: {
+            on: {
+                LOADED: "playing",
+            }
+        },
+        playing: {
+            on: {
+                PAUSE: 'paused',
+            }
+        },
+        paused: {
+            on: {
+                PLAY: 'playing'
+            }
+        },
+    },
+}
+
+function playerMachine(state, event) {
+    const nextStateValue = playerMachineObject.states[state.value].on?.[event.type];
+    if(!nextStateValue ) {
+        return state;
+    }
+    return {
+        ...state,
+        value: nextStateValue,
+    }
+}
+
+console.log(playerMachine(mac, { type: 'LOADED' }));
